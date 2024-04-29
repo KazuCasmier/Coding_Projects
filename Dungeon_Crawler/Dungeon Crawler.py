a2529592123a1d172085c54871f71b1693dec956
KazuCasmier <150320182+KazuCasmier@users.noreply.github.com>
2024-04-29 15:38:02 -0500

    More Additions
    Worked on the Dungeon Crawler more in my class today

--- Dungeon_Crawler/Dungeon Crawler.py
+++ Dungeon_Crawler/Dungeon Crawler.py
@@ -13,23 +13,27 @@ button_bg = '#B880F7'
 font = 'Arial'
 
 in_combat = False
-enemy_pos = {}
-player_pos = [0]
 
 """-Known_Bugs-
             
-        > Some of the enemy spawn are still buggy and will not display movement correctly
-            - Maybe not bugged entirely, pretty sure it has something to do with the try and except statements
+        > Whenever an enemy occupies a space previously occupied by another enemy there is a chance they will disappear
+          becoming white and will reappear after the player moves
             
         > Clearing the top right info frame is not working right now, will fix soon.
         
     -In_Development-
     
         > Combat system
-    
+            X Show the player is in combat
+            X Run system
+            ~ Show if the enemies are overlapping
+            - Player/Enemy health
+            - Player/Enemy Attack
+            
         > Working health bar/actions on bottom left frame
         
         > Refreshing the floor after reaching the exit
+            ~ Works but should be implemented on further
         
         > Some sort of API implementation into the combat system
     
@@ -40,16 +44,23 @@ def settings():
     return webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
 
-def game_start():
+def destroy_window():
     window.destroy()
+    game_start()
+
+
+def game_start():
     dungeon_floor = tk.Tk()
     dungeon_floor.configure(background=window_bg, cursor='dot')
     dungeon_floor.title('Dungeon Crawler')
     dungeon_floor.geometry('820x720')
 
+
+    enemy_pos = {}
+    player_pos = [0]
+
     t_l_frame = tk.Frame(dungeon_floor, width=600, height=500, background=text_frame_bg)
     t_l_frame.grid(row=0, column=0, padx=5, pady=5)
-
 
     """Creates the floor grid using a for loop and list comprehension
   - Nabbed some of this code from Chat GPT because I was pulling my hair out trying to make a modular grid"""
@@ -81,7 +92,7 @@ def game_start():
             if x == 4 and y == 4:
                 pass
             else:
-                labels[x][y].config(bg='Gold')
+                chest = labels[x][y].config(bg='Gold')
         for z in range(1):
             y = randint(0, 4)
             labels[0][y].config(bg='Blue')
@@ -94,10 +105,11 @@ def game_start():
 
     def enemy_movement():
         print(f'Before: {enemy_pos}')
-        for x in range(2):
+        for x in range(len(enemy_pos)):
+            if player_pos == enemy_pos[0] or player_pos == enemy_pos[1]:
+                combat()
             print(x)
             direction = randint(0, 1)
-            # print(f'Enemy: {x}, {direction}, {enemy_pos[x][1]}')
             labels[enemy_pos[x][0]][enemy_pos[x][1]].config(bg='White')
             # Direction 0 = x, 1 = y
             if direction == 0:
@@ -132,9 +144,12 @@ def game_start():
                     except IndexError:
                         enemy_pos[x][1] += 1
                         labels[enemy_pos[x][0]][enemy_pos[x][1]].config(bg='Red')
-        for enemy in range(2):
+        for enemy in range(len(enemy_pos)):
             if player_pos == enemy_pos[enemy]:
                 combat()
+        if enemy_pos[1] == enemy_pos[0]:
+            labels[enemy_pos[0][0]][enemy_pos[0][1]].config(bg='Dark Red')
+            labels[enemy_pos[1][0]][enemy_pos[1][1]].config(bg='Dark Red')
 
     def quit_game():
         quit()
@@ -142,20 +157,15 @@ def game_start():
     """This frame will display the instructions, damage values, and whether or not you are in combat or not"""
     t_r_frame = tk.Frame(dungeon_floor, width=200, height=600, border=10, background=text_frame_bg)
     t_r_frame.grid(row=0, column=1, padx=5, pady=5, ipadx=5)
-    text = tk.Label(t_r_frame, wraplength=200, text="Use the arrow keys below to move."
-                                                    "\n\n  Your goal is the gray square in the bottom left"
-                                                    "\n\n  There will be one \"chest\" that also randomly spawns"
-                                                    "\n\n  The red squares are enemies, run or fight do with them as you please.",
-                    font=(font, 10, "bold"), pady=20, bg=input_frame_bg,
-                    fg="Light Gray")
-    text.pack()
     t_r_frame.propagate(False)
 
     """The combat method... more later"""
+
     def combat():
         global in_combat
         in_combat = True
         turn_order = randint(1, 2)
+        labels[player_pos[0]][player_pos[1]].config(bg='teal')
         text_2 = tk.Label(t_r_frame, wraplength=200, text='\nYou are in combat!!',
                           font=(font, 10, "bold"), pady=20, bg=input_frame_bg,
                           fg="Light Gray")
@@ -173,6 +183,7 @@ def game_start():
         print(run_chance)
         if run_chance == 1:
             in_combat = False
+            # t_r_frame.winfo_children()
             text_2 = tk.Label(t_r_frame, wraplength=200, text='\nYou are out of combat!!',
                               font=(font, 10, "bold"), pady=20, bg=input_frame_bg,
                               fg="Light Gray")
@@ -189,8 +200,11 @@ def game_start():
     b_l_frame.grid(row=1, column=0, padx=5, pady=5)
 
     # HEALTH SLIDER!!!
+    player_hp = 100
     hp_slider = (ttk.Progressbar(b_l_frame, orient=tk.HORIZONTAL, length=400, mode="determinate")
-                 .grid(column=0, row=0, rowspan=2, ipady=10))
+                 .grid(column=0, row=0, rowspan=2, ipady=5))
+    hp_text = (tk.Label(b_l_frame, text=f'100/{player_hp}', font=font, bg=text_frame_bg)
+               .grid(column=0, row=1))
 
     attack_btn = (tk.Button(b_l_frame, text='ATTACK', bg=button_bg)
                   .grid(row=0, column=1, padx=25, pady=5))
@@ -218,10 +232,11 @@ def game_start():
                 y += 1
                 player_pos[1] = y
                 labels[player_pos[0]][player_pos[1]].config(bg='Blue')
-            for enemy in range(2):
+            for enemy in range(len(enemy_pos)):
                 if player_pos == enemy_pos[enemy]:
                     combat()
             enemy_movement()
+
         else:
             text_3 = tk.Label(t_r_frame, wraplength=200, text='You cannot move, you are in combat',
                               font=(font, 10, "bold"), pady=20, bg=input_frame_bg,
@@ -243,10 +258,14 @@ def game_start():
                 y -= 1
                 player_pos[1] = y
                 labels[player_pos[0]][player_pos[1]].config(bg='Blue')
-            for enemy in range(2):
+            if player_pos == [4, 4]:
+                dungeon_floor.destroy()
+                game_start()
+            for enemy in range(len(enemy_pos)):
                 if player_pos == enemy_pos[enemy]:
                     combat()
             enemy_movement()
+
         else:
             text_3 = tk.Label(t_r_frame, wraplength=200, text='You cannot move, you are in combat',
                               font=(font, 10, "bold"), pady=20, bg=input_frame_bg,
@@ -268,7 +287,10 @@ def game_start():
                 x -= 1
                 player_pos[0] = x
                 labels[player_pos[0]][player_pos[1]].config(bg='Blue')
-            for enemy in range(2):
+            if player_pos == [4, 4]:
+                dungeon_floor.destroy()
+                game_start()
+            for enemy in range(len(enemy_pos)):
                 if player_pos == enemy_pos[enemy]:
                     combat()
             enemy_movement()
@@ -293,7 +315,7 @@ def game_start():
                 x += 1
                 player_pos[0] = x
                 labels[player_pos[0]][player_pos[1]].config(bg='Blue')
-            for enemy in range(2):
+            for enemy in range(len(enemy_pos)):
                 if player_pos == enemy_pos[enemy]:
                     combat()
             enemy_movement()
@@ -324,14 +346,23 @@ window.title('Dungeon Crawler')
 window.geometry('700x600')
 title = tk.Label(window, text="Dungeon Crawler!!!", font=(font, 28, "bold"), pady=20, bg=input_frame_bg,
                  fg="Light Gray")
-start_button = tk.Button(window, text="Start", command=game_start, bg=button_bg, fg='Black', pady=20, width=100,
+start_button = tk.Button(window, text="Start", command=destroy_window, bg=button_bg, fg='Black', pady=20, width=100,
                          font=font)
 settings_button = tk.Button(window, text="Settings", command=settings, bg=button_bg, fg='Black', pady=20, width=100,
                             font=font)
+
+text = tk.Label(window, wraplength=700, text="Use the on screen arrow keys to move."
+                                             "\n\n -Your goal is the black square in the bottom right"
+                                             "\n\n -There will be one \"chest\" that also randomly spawns"
+                                             "\n\n -The red squares are enemies, run or fight do with them as you please.",
+                font=(font, 15, "bold"), pady=20, bg=input_frame_bg,
+                fg="Light Gray")
+
 exit_button = tk.Button(window, text="Exit", bg=button_bg, fg='Black', command=exit, font=font)
 title.pack(fill='x')
 start_button.pack(pady=20)
 settings_button.pack(pady=5)
+text.pack(fill='x')
 exit_button.pack(side='bottom', fill='x', ipady=15)
 
 window.mainloop()
